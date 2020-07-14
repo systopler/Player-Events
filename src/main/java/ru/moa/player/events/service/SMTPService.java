e5fc7d430931061b9c0cc941aa9f2f84f26effa9
systopler <systopler@rambler.ru>
2020-07-14 09:56:21 +0300

    SMTPService

---
+++ src/main/java/ru/moa/player/events/service/SMTPService.java
@@ -0,0 +1,117 @@
+package ru.moa.player.events.service;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.List;
+import java.util.Properties;
+
+@Service
+@Slf4j
+@Data
+public class SMTPService {
+    @Autowired
+    private Environment env;
+
+    private Properties props;
+
+    private String userName;
+    private String password;
+    private String mailFrom;
+
+    private boolean isTest;
+    private String mailToTest;
+
+    @PostConstruct
+    private void init(){
+        props = new Properties();
+
+        props.put("mail.smtp.host", env.getProperty("mail.smtp.host"));
+        props.put("mail.smtp.port", env.getProperty("mail.smtp.port"));
+        if (env.getProperty("mail.smtp.socketFactory.exists") != null && Boolean.parseBoolean(env.getProperty("mail.smtp.socketFactory.exists"))){
+            props.put("mail.smtp.socketFactory.class", env.getProperty("mail.smtp.socketFactory.class"));
+            props.put("mail.smtp.socketFactory.port", env.getProperty("mail.smtp.socketFactory.port"));
+        }
+        props.put("mail.smtp.auth", env.getProperty("mail.smtp.userName") != null ? "true" : "false");
+        props.put("mail.debug", "true");
+
+        if (env.getProperty("mail.encoding") != null){
+            props.put("mail.mime.charset", env.getProperty("mail.encoding"));
+        }
+
+        userName = env.getProperty("mail.smtp.userName");
+        password = env.getProperty("mail.smtp.password");
+        mailFrom = env.getProperty("mail.from");
+
+        isTest = Boolean.parseBoolean(env.getProperty("mail.test"));
+        if (env.getProperty("mail.test.to") != null){
+            mailToTest = env.getProperty("mail.test.to");
+        }
+    }
+
+    private Session getSession(){
+        log.debug("props: {}", props);
+        if (userName != null) {
+            return Session.getInstance(props,
+                    new javax.mail.Authenticator() {
+                        protected PasswordAuthentication getPasswordAuthentication() {
+                            return new PasswordAuthentication(userName, password);
+                        }
+                    }
+            );
+        } else {
+            return Session.getInstance(props);
+        }
+    }
+
+    public void send(
+            String subject,
+            String text,
+            String mailTo
+    ) throws MessagingException {
+
+        Session session = getSession();
+
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(mailFrom));
+
+            if (isTest) {
+                message.setRecipient(Message.RecipientType.TO, new InternetAddress(mailToTest));
+            } else {
+                message.setRecipient(Message.RecipientType.TO, new InternetAddress(mailTo));
+            }
+
+            message.setSubject(subject);
+            message.setText(text);
+
+            log.debug("message.getAllRecipients(): {}", message.getAllRecipients());
+            log.debug("message: {}", message);
+
+            Transport.send(message);
+
+            log.debug("send mail done");
+
+        } catch (MessagingException e) {
+            log.error(e.getLocalizedMessage());
+            throw new MessagingException(e.getMessage());
+        }
+    }
+
+    public void send(
+            String subject,
+            String text,
+            List<String> mailTo
+    ) throws MessagingException {
+        for (String item : mailTo) {
+            send(subject, text, item);
+        }
+    }
+}
