c5eb5e36b56fc24eb8a6febe17d4d14341a2f129
systopler <systopler@rambler.ru>
2020-07-21 07:22:01 +0300

    fileService

--- src/main/java/ru/moa/player/events/service/FileService.java
+++ src/main/java/ru/moa/player/events/service/FileService.java
@@ -12,4 +12,47 @@ public class FileService {
     private final FileRepository fileRepository;
 
     private final ServletContext context;
+
+    @Transactional(readOnly = true)
+    public FndFile findOne(Long id){
+        // TODO
+        return fndFileRepository.findById(id).get();
+    }
+
+    public FndFile save(MultipartFile inputFile) throws IOException {
+        FndFile fileInfo = new FndFile();
+
+        fileInfo.setFileName(FilenameUtils.removeExtension(FilenameUtils.getName(inputFile.getOriginalFilename())));
+        fileInfo.setSize(inputFile.getSize());
+        fileInfo.setMimeType(inputFile.getContentType());
+        fileInfo.setFileExt(FilenameUtils.getExtension(inputFile.getOriginalFilename()));
+        fileInfo = save(fileInfo);
+
+        // copy file
+        inputFile.transferTo(getFile(fileInfo));
+
+        return fileInfo;
+    }
+
+    private File getFile(FndFile fndFile) {
+        return new File(context.getRealPath("/files")+  File.separator + fndFile.getId() + "." +fndFile.getFileExt());
+    }
+
+    @Transactional
+    public FndFile save(FndFile fndFile){
+        return fndFileRepository.save(fndFile);
+    }
+
+    public void delete(Long id){
+        delete(findOne(id));
+    }
+
+    public void delete(FndFile fndFile){
+        File file = getFile(fndFile);
+        if (file.exists()){
+            file.delete();
+        }
+        fndFileRepository.delete(fndFile);
+    }
+
 }
